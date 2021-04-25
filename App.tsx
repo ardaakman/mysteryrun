3996cf59f65a40e702eecb4b066e5ffd6dc466a9
ArdaAkman <ardaakman16@gmail.com>
2021-04-24 19:14:46 -0700

    Designed the basics of the MainScreen

--- App.tsx
+++ App.tsx
@@ -4,13 +4,26 @@ import { createStackNavigator } from "@react-navigation/stack";
 import { SafeAreaProvider } from "react-native-safe-area-context";
 import { StyleSheet, Text, View } from "react-native";
 import { EntryStackScreen } from "./screens/EntryStackScreen";
+import {DefaultTheme, Provider} from 'react-native-paper'
 
 const Stack = createStackNavigator();
 
+const theme = {
+  ...DefaultTheme,
+  roundness: 2,
+  colors: {
+    ...DefaultTheme.colors,
+    primary: '#FAE1A0',
+    accent: '#FF52BA',
+  },
+};
+
 export default function App() {
   return (
+    <Provider theme = {theme}>
     <SafeAreaProvider>
       <EntryStackScreen/>
     </SafeAreaProvider>
+    </Provider>
   );
 }
--- screens/MainStack/MainScreen/MainScreen.tsx
+++ screens/MainStack/MainScreen/MainScreen.tsx
@@ -1,14 +1,85 @@
 import React, { useEffect, useState } from "react";
-import {View, Text, Button,  Dimensions, Image, StyleSheet} from "react-native";
+import {View, Text,  Dimensions, Image, StyleSheet} from "react-native";
 import MapView, { Marker } from "react-native-maps";
 import { greaterThan } from "react-native-reanimated";
+import { Appbar, Card, Button, Headline } from "react-native-paper";
+import Icon from 'react-native-vector-icons/FontAwesome5';
 
+
+
+const anyanyprint = () => {
+    console.warn("pressed")
+}
+const settingsIcon = (
+    <Icon.Button
+      name="puzzle-piece"
+      backgroundColor="#FAE1A0"
+      onPress = {anyanyprint}
+    >
+      Puzzles
+    </Icon.Button>
+  );
+
+  const leaderboardIcon = (
+    <Icon.Button
+      name="leaderboard"
+      backgroundColor="#FAE1A0"
+      onPress = {anyanyprint}
+    >
+
+    </Icon.Button>
+  );
+
+
+const BottomBar = () => {
+    return (
+        <Appbar style ={styles.bottom}>
+            <Appbar.Action
+            icon = "cog-outline"
+            />
+
+            <Appbar.Action
+            icon = 'poll'
+            />
+
+            <Appbar.Action
+            icon = 'newspaper-variant'
+            />
+
+        </Appbar>
+    )
+} 
+
+
+const TopBar = () => {
+    return (
+        <Appbar style ={styles.top}>
+                <Appbar.Content title = 'Mystery Run'/>
+
+        </Appbar>
+    )
+} 
 export function MainScreen() {
     return (
     <View>
-        <Text>
-            Nothing Here
-        </Text>
+        {BottomBar()}
+        {TopBar()}
     </View>
     )
 }
+
+const styles = StyleSheet.create({
+    bottom: {
+      position: 'absolute',
+      left: 0,
+      right: 0,
+      top: 640
+    },
+    top : {
+        position : "absolute",  
+        left: 0,
+        right: 0,
+        top: 0,
+        
+    }
+  });
