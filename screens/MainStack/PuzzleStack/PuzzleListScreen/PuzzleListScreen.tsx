dcd36c5888548c8f84d6787850a2786ac0a0c00d
Dominic de Bettencourt <dominicd@berkeley.edu>
2021-05-04 18:59:57 -0700

    added sliding panel

--- screens/MainStack/PuzzleStack/PuzzleListScreen/PuzzleListScreen.tsx
+++ screens/MainStack/PuzzleStack/PuzzleListScreen/PuzzleListScreen.tsx
@@ -1,34 +1,52 @@
 import { View, Text, Dimensions, Image, StyleSheet } from "react-native";
-import React from "react"
-import { Appbar, Card, Button, Headline, Paragraph, Title } from "react-native-paper";
+import React from "react";
+import {
+  Appbar,
+  Card,
+  Button,
+  Headline,
+  Paragraph,
+  Title,
+} from "react-native-paper";
 import Icon from "react-native-vector-icons/FontAwesome5";
 import { TouchableOpacity } from "react-native";
-import {styles, imageUrl} from "./PuzzleListScreenStyles"
+import { styles, imageUrl } from "./PuzzleListScreenStyles";
+import SlidingUpPanel from "rn-sliding-up-panel";
 
-
-export function PuzzleListScreen ({navigation}) {
-
-
+export function PuzzleListScreen({ navigation }) {
   const TopBar = () => {
     return (
-      <Appbar style = {styles.topPuzzles}>
-        <Appbar.Action icon = 'arrow-left' onPress = {() => navigation.goBack()}/>
-        <Appbar.Content style = {styles.header} title = {"List Of Puzzles"}/>
+      <Appbar style={styles.topPuzzles}>
+        <Appbar.Action icon="arrow-left" onPress={() => navigation.goBack()} />
+        <Appbar.Content style={styles.header} title={"List Of Puzzles"} />
       </Appbar>
-    )
+    );
+  };
 
-  }
- 
-    return (
-    <View style = {styles.container}>
+  return (
+    <View style={styles.container}>
       {TopBar()}
-        <Card onPress = {()=> {navigation.navigate("PuzzleMapScreen")}} style = {styles.card}>
-          <Card.Content>
-            <Card.Cover source = {{uri:imageUrl}}/>
-            <Title> {'Oski Trail'} </Title>
-            <Paragraph> {'A thrilling puzzle that takes around the UC Berkeley campus.' }</Paragraph>
-          </Card.Content>
-        </Card>
+      <Card
+        onPress={() => {
+          navigation.navigate("PuzzleMapScreen");
+        }}
+        style={styles.card}
+      >
+        <Card.Content>
+          <Card.Cover source={{ uri: imageUrl }} />
+          <Title> {"Oski Trail"} </Title>
+          <Paragraph>
+            {" "}
+            {"A thrilling puzzle that will take you around North Berkeley."}
+          </Paragraph>
+        </Card.Content>
+      </Card>
+      <SlidingUpPanel>
+        <View style={styles.container}>
+          <Text>Here is the content inside panel</Text>
+          <Button title="Hide" onPress={2} />
+        </View>
+      </SlidingUpPanel>
     </View>
-    )
+  );
 }
--- screens/MainStack/PuzzleStack/PuzzleMapScreen/PuzzleMapScreen.tsx
+++ screens/MainStack/PuzzleStack/PuzzleMapScreen/PuzzleMapScreen.tsx
@@ -12,9 +12,11 @@ import {
 import MapView, { Marker } from "react-native-maps";
 import { greaterThan } from "react-native-reanimated";
 import * as Location from "expo-location";
+import SlidingUpPanel from "rn-sliding-up-panel";
+import { listStyles } from "../PuzzleListScreen/PuzzleListScreenStyles";
 
 export default function PuzzleMapScreen({ navigation, route }) {
-  const [location, setLocation] = useState("");
+  const [location, setLocation] = useState({});
   const [errorMsg, setErrorMsg] = useState("");
 
   useEffect(() => {
@@ -36,6 +38,22 @@ export default function PuzzleMapScreen({ navigation, route }) {
   } else if (location) {
     text = JSON.stringify(location);
   }
+  class SlidingPanel extends React.Component {
+    render() {
+      return (
+        <View style={styles.slideStyle}>
+          <SlidingUpPanel ref={(c) => (this._panel = c)}>
+            <View style={styles.container}>
+              <Text style={styles.headerTextStyle}>Puzzle Information</Text>
+              <Text style={styles.puzzleTextStyle}>
+                Please proceed to the location on the map to begin.
+              </Text>
+            </View>
+          </SlidingUpPanel>
+        </View>
+      );
+    }
+  }
 
   return (
     <View>
@@ -49,7 +67,14 @@ export default function PuzzleMapScreen({ navigation, route }) {
           latitudeDelta: 0.0922,
           longitudeDelta: 0.0421,
         }}
-      ></MapView>
+      >
+        <Marker
+          coordinate={{ latitude: 37.8703, longitude: -122.2595 }}
+          title={"Begin the Puzzle"}
+          description={"Go here to begin the puzzle!"}
+        />
+      </MapView>
+      <SlidingPanel />
     </View>
   );
 }
@@ -66,7 +91,7 @@ const styles = StyleSheet.create({
   },
   mapStyle: {
     width: Dimensions.get("window").width,
-    height: Dimensions.get("window").height,
+    height: Dimensions.get("window").height * 0.8,
   },
   headerStyle: {
     backgroundColor: "#003262",
@@ -78,5 +103,37 @@ const styles = StyleSheet.create({
     fontSize: 20,
     textAlign: "center",
     margin: 10,
+  },
+  opacityStyle: {
+    position: "absolute",
+    width: 80,
+    height: 100,
+  },
+  container: {
+    flex: 1,
+    backgroundColor: "white",
+    alignItems: "center",
+    //justifyContent: "center",
+  },
+  slideStyle: {
+    position: "absolute",
+    bottom: 50,
+    width: "100%",
+    alignItems: "center",
+    padding: 20,
+  },
+  headerTextStyle: {
+    fontFamily: "Roboto",
+    fontSize: 30,
+    fontWeight: "500",
+    width: "100%",
+    justifyContent: "center",
+    textAlign: "center",
+    backgroundColor: "yellow",
+  },
+  puzzleTextStyle: {
+    fontFamily: "Roboto",
+    fontSize: 12,
+    marginTop: 300,
   },
 });
